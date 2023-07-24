20a15899918b5fbd3c777549c0539a1489d6bd05
AyanSwarga <ayannicholas@gmail.com>
2023-07-24 18:28:47 +0100

    tasks

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
-"""Start web application with two routings
-"""
-
+""" This Script starts a Flask web application """
 from flask import Flask
+
+
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def hello():
-    """Return string when route queried
-    """
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb')
-def hbnb():
-    """Return string when route queried
-    """
+@app.route('/hbnb', strict_slashes=False)
+def holberon():
     return 'HBNB'
 
-if __name__ == '__main__':
-    app.url_map.strict_slashes = False
+
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
