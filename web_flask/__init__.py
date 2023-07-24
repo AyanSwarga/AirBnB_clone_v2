f60358937342224bd7236985ccd6be96f82afc49
AyanSwarga <ayannicholas@gmail.com>
2023-07-24 20:08:59 +0100

    tasks

--- web_flask/__init__.py
+++ web_flask/__init__.py
@@ -1,11 +1,10 @@
-
 m flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello HBNB!'
+        return 'Hello HBNB!'
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    if __name__ == '__main__':
+            app.run(host='0.0.0.0', port=5000, debug=True))
